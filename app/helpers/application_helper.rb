ee423f2f79145eb01cc49b76a68fb0f3fdbceab9
Joseph <jvg_0119@yahoo.com>
2017-01-06 21:35:45 -0800

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,15 @@
+module ApplicationHelper
+
+	def flash_class(type)
+    case type
+      when "success" then "alert alert-success" # green
+      when "info" then "alert alert-info"       # blue
+      when "notice" then "alert alert-info"     # blue
+      when "warning" then "alert alert-warning" # yellow
+      when "alert" then "alert alert-warning"   # yellow
+      when "danger" then "alert alert-danger"   # red
+      when "error" then "alert alert-danger"    # red
+    end
+  end
+
+end
